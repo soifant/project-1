5dfc95d2c6fedb28cc470ebd3716f0fb0a99e098
soifant <31540006+soifant@users.noreply.github.com>
2022-08-22 21:10:46 +0700

    Add controller.php

---
+++ App/core/controller.php
@@ -0,0 +1,25 @@
+<?php
+
+class Controller{
+
+	public function view($view, $data){
+		require_once '../app/view/'.$view.'.php';
+	
+	}
+	
+	public function model($model){
+		require_once '../app/model/'.$model.'.php';
+		return new $model;
+	
+	}
+
+
+
+}
+
+
+
+
+
+
+?>
