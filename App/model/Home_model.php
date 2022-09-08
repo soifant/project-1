5fc5cc18018c1b89b0a9d1684599116712e99186
soifant <31540006+soifant@users.noreply.github.com>
2022-09-08 23:10:45 +0700

    Update Home_model.php

--- App/model/Home_model.php
+++ App/model/Home_model.php
@@ -8,21 +8,54 @@ class Home_model{
 	}
 	
 	
-	public function getArtikel(){
-		$query = "SELECT * FROM data_artikel";
+	public function getArtikel($nama){
+		$query = "SELECT * FROM data_tutorial where judul=:judul and tutorial=:tutorial and status=:status";
 		$this->db->query($query);
+		$this->db->bind("judul", str_replace('-', ' ', $nama));
+		$this->db->bind("tutorial", "artikel");
+		$this->db->bind('status', 'publik');
 		return $this->db->ambilData();
 	
 	}
 	
-	public function getPost($link){
-		$query = "SELECT * FROM data_artikel WHERE title=:title";
+	public function getVideo($nama){
+		$query = "SELECT * FROM data_tutorial where judul=:judul and tutorial=:tutorial and status=:status";
 		$this->db->query($query);
-		$this->db->bind("title", $link);
+		$this->db->bind("judul", str_replace('-', ' ', $nama));
+		$this->db->bind("tutorial", "video");
+		$this->db->bind('status', 'publik');
+		return $this->db->ambilData();
+		
+	}
+	
+	public function getContoh($nama){
+		
+		$query = "SELECT * FROM data_tutorial WHERE judul=:judul and tutorial=:tutorial and status=:status";
+		$this->db->query($query);
+		$this->db->bind("judul", str_replace('-', ' ', $nama));
+		$this->db->bind("tutorial", "contoh");
+		$this->db->bind('status', 'publik');
 		return $this->db->ambilData();
 	}
 	
-
+	public function getTutorial($nama){
+	
+		$query = "SELECT DISTINCT judul FROM data_tutorial WHERE kategori=:kategori and status=:status";
+		$this->db->query($query);
+		$this->db->bind('kategori', $nama);
+		$this->db->bind('status', 'publik');
+		return $this->db->ambilData();
+	}
+	
+	public function getAll(){
+	
+	$query = "SELECT DISTINCT kategori FROM data_tutorial";
+	$this->db->query($query);
+	return $this->db->ambilData();
+	}
+	
+	
+	
 
 }
 
