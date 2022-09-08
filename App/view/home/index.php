9e7fb7ebe76aaad3d7fbf62544596193ab3b2052
soifant <31540006+soifant@users.noreply.github.com>
2022-09-08 23:13:33 +0700

    Update index.php

--- App/view/home/index.php
+++ App/view/home/index.php
@@ -1,31 +1,73 @@
-<?php
-foreach($data['artikel'] as $r):
-?>
 
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title"><?= $r['title']; ?></h5>
-    <p class="card-text"><?= $r['deskripsi']; ?></p>
-    <a href="<?= $r['link']; ?>" class="card-link">Selengkapnya</a>
-  </div>
+
+
+<div class="container-fluid bg-success white p-5">
+<center>
+<h3 class="mb-3">
+Temukan semua tutorial pemrograman dalam bentuk artikel, video dan contoh kode
+</h3>
+
+<small>
+Kita digunakan banyak orang untuk belajar pemrograman
+</small>
+
+<br>
+<button class="btn btn-dark mt-3">
+Mulai Belajar
+</button>
+</center>
 </div>
 
-<?php endforeach;?>
 
-<nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  </ul>
-</nav>
+
+<div class="container mt-4 mb-5 intro">
+
+<div class="row p-4">
+<b class="p-0">
+<i class="fa-brands fa-readme"></i>Baca artikel
+</b>
+Baca artikel dari berbagai situs profesional untuk menaikan skill coding.
+</div>
+
+<div class="row p-4">
+<b class="p-0">
+<i class="fa-brands fa-youtube"></i>Tonton video
+</b>
+Tonton video dari profesional programer untuk memudahkan dalam belajar.
+</div>
+
+<div class="row p-4">
+<b class="p-0">
+<i class="fas fa-laptop-code"></i>
+Contoh kode
+</b>
+Temukan banyak contoh kode dan kolaborasikan dengan code kamu.
+</div>
+
+</div>
+
+
+<div class="container mb-5">
+
+<center>
+<?php foreach($data['all'] as $a) :?>
+<div class="card shadow-sm mb-3" style="width: 19.5rem;">
+  <div class="card-body">
+    <h2 class="card-title">
+    
+    <li class="fa-brands fa-html5"> <?= $a['kategori'] ;?></li></h2>
+    <p class="card-text">
+    Kumpulan tutorial <?= $a['kategori'] ;?> artikel, video dan contoh kode</p>
+    <a href="/?url=home/page/<?= $a['kategori'] ;?>">
+    <button class="btn btn-sm btn-success white"><b>Pelajari</b></button></a>
+    
+    </a>
+  </div>
+</div>
+<?php endforeach ;?>
+
+</center>
+</div>
+
+
+
