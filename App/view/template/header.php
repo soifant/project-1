ee0cb9d1aac1e3f4c6bcadcc7e531f1708678e43
soifant <31540006+soifant@users.noreply.github.com>
2022-09-08 23:11:44 +0700

    Update header.php

--- App/view/template/header.php
+++ App/view/template/header.php
@@ -1,9 +1,77 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content='width=device-width, initial-scale=1' name='viewport'/>
+    
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style type="text/css">
+    .fab{
+    font-size:60px;
+   
+    }
+    .warning{
+   color: #ffc107;
+    }
+    .w2{
+    color: #806B00;
+    }
+    .white{
+    color:white;
+    }
+    .intro i{
+    font-size:20px;
+    margin-right:5px;
+    
+    }
+    .bg-hijau-gelap{
+    background:#CCCCCC;
+    }
+    .code{
+    color:green;
+    }
+    .contoh-code{
+    padding:10px;
+    background:#ccc;
+    border-left:3px solid green;
+    margin:5px;
+    margin-bottom:20px;
+    }
+    </style>
     <title>Hello, world!</title>
   </head>
   <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/"><b>KursusWEB</b></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="/?url=home/page/html">Html</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/?url=home/page/css">Css</a>
+        </li>
+        
+        <li class="nav-item">
+        <a class="nav-link" href="/?url=home/page/javascript">Javascript</a>
+        </li>
+        
+        <li class="nav-item">
+        <a class="nav-link" href="/?url=home/page/bootstrap">Bootstrap</a>
+        </li>
+        
+        <li class="nav-item">
+        <a class="nav-link" href="/?url=admin">Admin</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-dark" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
