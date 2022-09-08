24c58f7c12ddd98ccf0423ece32ed4f8c401b803
soifant <31540006+soifant@users.noreply.github.com>
2022-09-08 23:12:37 +0700

    Update footer.php

--- App/view/template/footer.php
+++ App/view/template/footer.php
@@ -1,3 +1,29 @@
+<div class="container-fluid p-4"><center>
+<i><b>"</b>IamCode lebih baik dari semua platfome belajar pemrograman, dan kami sangat mudah memahami semua tutorialnya.<b>"</b>
+</i><br>
+<b>
+Programer otodidak
+</b>
+
+</center>
+</div>
+
+<div class="container-fluid bg-dark white mt-4 p-4 pb-2">
+<small>
+<center>
+<a class="text-decoration-none white m-1" href="About" >Tentang</a>
+<a class="text-decoration-none white m-1" href="Contack" >Hubungi</a>
+<a class="text-decoration-none white m-1" href="Disclaimer" >Perivasi</a> 
+<a class="text-decoration-none white" href="Term" >Persaratan</a>
+
+<br>
+
+<b>
+iamcode.xyz</b> © 2022
+</center>
+</small>
+</div>
+<script src="https://kit.fontawesome.com/7593115e85.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
