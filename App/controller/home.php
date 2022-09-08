8a4e92e062cf5db908d17b9cf442954fa8883bdf
soifant <31540006+soifant@users.noreply.github.com>
2022-09-08 23:09:46 +0700

    Update home.php

--- App/controller/home.php
+++ App/controller/home.php
@@ -1,20 +1,33 @@
 <?php
 
-class home{
+class home extends Controller{
 	public function index(){
-		$data['artikel'] = $this->model('Home_model')->getArtikel();
+		$data['all'] = $this->model('Home_model')->getAll();
 		$this->view('template/header');
 		$this->view('home/index', $data);
 		$this->view('template/footer');
 	}
 	
+	public function page($nama){
+		$data['tutorial'] = $this->model('Home_model')->getTutorial($nama);
+		$data['all'] = $this->model('Home_model')->getAll();
+		$data['title'] = $nama;
+		$this->view('template/header');
+		$this->view('home/page', $data);
+		$this->view('template/footer');
+	}
+	
 	public function post($link){
-		$data['artikel'] = $this->model('Home_model')->getPost($link);
+		$data['artikel'] = $this->model('Home_model')->getArtikel($link);
+		$data['video'] = $this->model('Home_model')->getVideo($link);
+		$data['contoh'] = $this->model('Home_model')->getContoh($link);
+		$data['title'] = str_replace('-', ' ', $link);
 		$this->view('template/header');
 		$this->view('home/post', $data);
 		$this->view('template/footer');
 	}
-
+	
+	
 }
 
 
