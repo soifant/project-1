2fa512b32e22036af78fa4e695aefb061ee9c993
soifant <31540006+soifant@users.noreply.github.com>
2022-09-08 23:21:42 +0700

    Create tambah-post.php

---
+++ App/view/admin/tambah-post.php
@@ -0,0 +1,11 @@
+<div class="row m-4">
+  <form method="post" action="/?url=admin/tambah" >
+  <input name="judul" placeholder="Judul"  class="form-control border border-dark mb-2">
+  <input name="sub_judul" placeholder="Sub-Judul" class="form-control border border-dark mb-2">
+  <input name="link" placeholder="link" class="form-control border border-dark mb-2">
+  <input name="dari" placeholder="dari" class="form-control border border-dark mb-2">
+  <input name="kategori" placeholder="Kategori" class="form-control border border-dark mb-2">
+  <input name="tutorial" placeholder="Tutorial" class="form-control border border-dark mb-2">
+  <button class="btn btn-block btn-success">Tambah tutorial</button>
+  </form>
+  </div>
