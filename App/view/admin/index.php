8e5d19218342640a9c6d8098c106a23ae01ea2ee
soifant <31540006+soifant@users.noreply.github.com>
2022-09-10 21:40:08 +0700

    Update index.php

--- App/view/admin/index.php
+++ App/view/admin/index.php
@@ -1,21 +1,39 @@
-
-<div class="row p-2 m-3">
-<a href="/?url=admin/tambahPost" >
-<button class="btn btn-block btn-success">Tambah artikel</button>
+<div class="container">
+<div>
+<a href="/?url=admin/tambahPost" class="text-decoration-none link card bg-light mt-4 p-2">
+<center><small>Tambah tutorial</small></center>
 </a>
 </div>
-
-<div class="row p-2 m-3 mt-5">
-<h3 class="p-0">
-Semua tutorial
-</h3>
-
-<ul>
-<?php foreach($data['tutorial'] as $h) : ?>
-<li><a href="/?url=admin/post/<?= str_replace(' ', '-', trim($h['judul'])) ;?>" class="text-decoration-none" >
-<?= $h['judul'] ;?></a>(<?= $h['kategori'];?>)
-</li>
-<?php endforeach ;?>
-</ul>
+<table class="table table-striped mt-3 border">
+  <thead>
+    <tr>
+       <th scope="col">No</th>
+      <th scope="col">Kategori</th>
+      <th scope="col">Artikel</th>
+      <th scope="col">Video</th>
+      <th scope="col">Contoh</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    
+    <?php 
+    $x=1;
+    foreach($data['tutorial'] as $h) : ?>
+    <tr>
+    <th scope="row"><?= $x++ ;?></th>
+      <td><a class="text-decoration-none link" href="/?url=admin/page/<?= $h['kategori'];?>">
+      <?= $h['kategori'] ;?>
+      </a></td>
+      <td>Null</td>
+      <td>Null</td>
+      <td>Null</td>
+      </tr>
+      <?php endforeach ;?>
+     
+  </tbody>
+</table>
 </div>
+
+
 
