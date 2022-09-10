5d1ff281dd590809d6fbd7d8935a8a1857b2c38c
soifant <31540006+soifant@users.noreply.github.com>
2022-09-10 13:57:31 +0700

    Update index.php

--- App/view/home/index.php
+++ App/view/home/index.php
@@ -1,73 +1,37 @@
-
-
-
-<div class="container-fluid bg-success white p-5">
-<center>
-<h3 class="mb-3">
-Temukan semua tutorial pemrograman dalam bentuk artikel, video dan contoh kode
-</h3>
-
-<small>
-Kita digunakan banyak orang untuk belajar pemrograman
-</small>
-
-<br>
-<button class="btn btn-dark mt-3">
-Mulai Belajar
-</button>
-</center>
-</div>
-
-
-
-<div class="container mt-4 mb-5 intro">
-
-<div class="row p-4">
-<b class="p-0">
-<i class="fa-brands fa-readme"></i>Baca artikel
-</b>
-Baca artikel dari berbagai situs profesional untuk menaikan skill coding.
-</div>
-
-<div class="row p-4">
-<b class="p-0">
-<i class="fa-brands fa-youtube"></i>Tonton video
-</b>
-Tonton video dari profesional programer untuk memudahkan dalam belajar.
-</div>
-
-<div class="row p-4">
-<b class="p-0">
-<i class="fas fa-laptop-code"></i>
-Contoh kode
-</b>
-Temukan banyak contoh kode dan kolaborasikan dengan code kamu.
-</div>
-
-</div>
-
-
 <div class="container mb-5">
 
-<center>
-<?php foreach($data['all'] as $a) :?>
-<div class="card shadow-sm mb-3" style="width: 19.5rem;">
-  <div class="card-body">
-    <h2 class="card-title">
-    
-    <li class="fa-brands fa-html5"> <?= $a['kategori'] ;?></li></h2>
-    <p class="card-text">
-    Kumpulan tutorial <?= $a['kategori'] ;?> artikel, video dan contoh kode</p>
-    <a href="/?url=home/page/<?= $a['kategori'] ;?>">
-    <button class="btn btn-sm btn-success white"><b>Pelajari</b></button></a>
-    
-    </a>
-  </div>
+<div class="p-1 py-4 mb-3 border-bottom">
+<b>Gocode.pw</b> Adalah situs untuk mencari tutorial pemrograman dalam bentuk artikel, video dan contoh kode.
+
+<div class="input-group mt-3 p-0">
+  <input type="text" class="form-control" placeholder="Cari Tutorial" aria-label="Cari Tutorial" aria-describedby="button-addon2">
+  <button class="btn btn-dark" type="button" id="button-addon2">Cari</button>
 </div>
+
+</div>
+
+
+<div class="row p-3 pb-0 mb-0">
+<h4 class="p-0">
+Pilih Tutorial
+</h4>
+</div>
+
+<div class="container" >
+
+<?php foreach($data['all'] as $a) :?>
+<div class="row p-3 mb-2 bg-light">
+
+<b class="p-0">
+<a href="?url=home/page/<?= $a['kategori'] ;?>" class="p-0 text-decoration-none link">
+<?= $a['kategori'] ;?>
+</a>
+</b>
+<small class="p-0">
+Kumpulan tutorial artikel, video dan contoh <?= $a['kategori'] ;?> tahun 2022
+</small></div>
 <?php endforeach ;?>
 
-</center>
 </div>
-
-
+</div>
 
