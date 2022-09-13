04a732fd98d850a730312cf23f177fbc6100f75b
soifant <31540006+soifant@users.noreply.github.com>
2022-09-13 19:00:38 +0700

    Update tambah-post.php

--- App/view/admin/tambah-post.php
+++ App/view/admin/tambah-post.php
@@ -2,7 +2,7 @@
 
 <!-- Tambah kategori -->
 <?php if($data['page'] === 'kategori') : ?>
-<form method="post" action="/?url=admin/tambah" >
+<form method="post" action="/?url=admin/tambah/kategori" >
   <input name="nama" placeholder="nama"  class="form-control border border-dark mb-2">
 <button class="btn btn-block btn-dark">Tambah kategori</button>
 </form>
@@ -10,16 +10,11 @@
 
 <!-- Tambah tutorial -->
 <?php if($data['page'] === 'tutorial') : ?>
-<form method="post" action="/?url=admin/tambah" >
-  <input name="nama" placeholder="nama"  class="form-control border border-dark mb-2">
+<form method="post" action="/?url=admin/tambah/tutorial" >
+  <input name="judul" placeholder="nama"  class="form-control border border-dark mb-2">
+<input name="kategori" value="<?= $data['id_kategori'] ;?>" class="form-control border border-dark mb-2">
 
-<label for="kategori">Kategori :</label>
-  <select class="p-1" id="kategori">
-  <option value="html">Html</option>
-  <option value="css">Css</option>
-  <option value="js">Js</option>
-  <option value="php">PHP</option>
-  </select>
+
   <br>
   <button class="mt-2 btn btn-block btn-dark">Tambah tutorial</button>
 </form>
@@ -27,27 +22,20 @@
 
 <!-- Tambah post -->
 <?php if($data['page'] === 'post') : ?>
-  <form method="post" action="/?url=admin/tambah" >
+  <form method="post" action="/?url=admin/tambah/post" >
   <input name="judul" placeholder="Judul"  class="form-control border border-dark mb-2">
    <input name="link" placeholder="link" class="form-control border border-dark mb-2">
   <input name="dari" placeholder="dari" class="form-control border border-dark mb-2">
+  <input name="tutorial" value="<?= $data['id_tutorial'] ;?>" class="form-control border border-dark mb-2">
+  <input name="kategori" value="<?= $data['id_kategori'] ;?>" class="form-control border border-dark mb-2">
   
+  <label for="type">Type :</label>
+  <select name="type" class="p-1" id="type">
+	  <option value="artikel">Artikel</option>
+	  <option value="video">Video</option>
+	  <option value="contoh">Contoh</option>
+    </select>
   
-  <label for="kategori">Kategori :</label>
-  <select class="p-1" id="kategori">
-  <option value="html">Html</option>
-  <option value="css">Css</option>
-  <option value="js">Js</option>
-  <option value="php">PHP</option>
-  </select>
-  
-  <label for="judul">Judul :</label>
-  <select class="p-1" id="judul">
-  <option value="html">Html</option>
-  <option value="css">Css</option>
-  <option value="js">Js</option>
-  <option value="php">PHP</option>
-  </select>
   
   <br>
   <button class="btn btn-block btn-dark mt-2">Tambah post</button>
